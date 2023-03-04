1b8c411639c605612ef8fbe84b687e50abd3ed01
sarah <sarah@SALIEN>
2023-03-04 15:02:05 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿var OFF = new DateTime(2023, 3, 4, 16, 0, 0);
+
+
+do
+{
+    DateTime now = DateTime.Now;
+    var timeleft = OFF - now;
+    Console.WriteLine(timeleft.TotalMinutes.ToString("N2"));
+    Console.Beep();
+    Thread.Sleep(1000);
+} while(true);
